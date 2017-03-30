46aac0958e8a620c46925180c9ab955f5f085805
hollygoat <siyuanli0185@gmail.com>
2017-03-30 00:02:56 -0700

    client can fetch(get) news from server

--- client/src/NewsPanel/NewsPanel.js
+++ client/src/NewsPanel/NewsPanel.js
@@ -15,27 +15,15 @@ class NewsPanel extends React.Component{
     }
 
     loadMoreNews(){
-        this.setState({
-            news: [
-                {'url':'http://us.cnn.com/2017/02/15/politics/andrew-puzder-failed-nomination/index.html',
-                    'title':"Inside Andrew Puzder's failed nomination",
-                    'description':"In the end, Andrew Puzder had too much baggage -- both personal and professional -- to be confirmed as President Donald Trump's Cabinet.",
-                    'source':'cnn',
-                    'urlToImage':'http://i2.cdn.cnn.com/cnnnext/dam/assets/170215162504-puzder-trump-file-super-tease.jpg',
-                    'digest':"3RjuEomJo26O1syZbU7OHA==\n",
-                    'reason':"Recommend"
-                },
-                {'title': 'Zero Motorcycles CTO Abe Askenazi on the future of two-wheeled EVs',
-                    'description': "Electric cars and buses have already begun to take over the world, but the motorcycle industry has been much slower to put out all-electric and hybrid models...",
-                    'url': "https://techcrunch.com/2017/03/23/zero-motorcycles-cto-abe-askenazi-on-the-future-of-two-wheeled-evs/",
-                    'urlToImage': "https://tctechcrunch2011.files.wordpress.com/2017/03/screen-shot-2017-03-23-at-14-04-01.png?w=764&h=400&crop=1",
-                    'source': 'techcrunch',
-                    'digest':"3RjuEomJo26O1syZbUdOHA==\n",
-                    'time':"Today",
-                    'reason':"Hot"
-                }
-            ]
-        });
+        fetch('http://localhost:3000/news', {
+            method: 'GET',
+            mode: 'cors',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            cache: 'no-cache'
+        }).then((res) =>  res.json())
+            .then((res) => {
+                this.setState( {news: this.state.news ? this.state.news.concat(res.news):res.news} );
+            });
     }
 
     renderNews(){
--- server/app.js
+++ server/app.js
@@ -3,17 +3,27 @@ var express = require('express');
 var app = express();
 
 var index = require('./routes/index');
+var news = require('./routes/news');
 
 app.set('views', path.join(__dirname, '../client/build/'));  // TODO: how actually use this
 app.set('view engine', 'jade'); // TODO: how actually use this
+
+// TODO: for purpose of developing client and server at same time, delete after client finished developement
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
+app.use('/news/', news);
 app.use('/', index);
 
 app.use(function(req, res) {
     // catch 404 and forward to error handler
     var err = new Error('Not Found');
     err.status = 404;
-    res.send('404 Not Found');
+    res.status(404).send('404 Not Found');
 });
 
 module.exports = app;
